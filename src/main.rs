11bd06fadfb7e712d5feec73f531b59df35c7e57
Marcus Vinícius Belfort <marvinbelfort@gmail.com>
2024-03-27 16:50:44 -0300

    Insert Dispatcher: Tablet Buttons

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use rusb::{
     devices, ConfigDescriptor, Device, DeviceHandle, Error as RusbError, GlobalContext,
     InterfaceDescriptor, TransferType,
@@ -8,14 +11,94 @@ use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
+use std::{u16, u8};
 
 const VID: u16 = 0x08f2;
 const PID: u16 = 0x6811;
 
+enum PressState {
+    Pressed = 0,
+    Release = 1,
+    Hold = 2,
+    None = 3,
+}
+
+struct TabletButton {
+    id: u8,
+    state: PressState,
+}
+
+struct DeviceDispatcher {
+    tablet_last_buttons: u16,
+    pen_buttons: u8,
+}
+
+impl DeviceDispatcher {
+    const X_AXIS_HIGH: usize = 1;
+    const X_AXIS_LOW: usize = 2;
+    const Y_AXIS_HIGH: usize = 3;
+    const Y_AXIS_LOW: usize = 4;
+    const Y_PRESSURE_HIGH: usize = 5;
+    const Y_PRESSURE_LOW: usize = 6;
+    const PEN_BUTTONS: usize = 9;
+    const TABLET_BUTTONS_HIGH: usize = 12;
+    const TABLET_BUTTONS_LOW: usize = 11;
+
+    pub fn new() -> Self {
+        DeviceDispatcher {
+            tablet_last_buttons: 0,
+            pen_buttons: 0,
+        }
+    }
+
+    pub fn dispatch(&mut self, buffer: &[u8]) -> () {
+        let binary_button_flags = Self::button_flags(
+            buffer[Self::TABLET_BUTTONS_HIGH],
+            buffer[Self::TABLET_BUTTONS_LOW],
+        );
+
+        let buttons = self.binary_to_buttons(binary_button_flags);
+        self.tablet_last_buttons = binary_button_flags;
+
+        // Mapeia botões pressionados para KEY_Events
+        // Calcula boundaries e Emite X, Y
+        // Calcula e emite Pen Pressure
+    }
+
+    fn button_flags(high: u8, low: u8) -> u16 {
+        ((high | 0xcc) as u16) << 8 | low as u16
+    }
+
+    fn binary_to_buttons(&self, binary_button_flags: u16) -> Vec<TabletButton> {
+        (0..14)
+            .filter(|i| ![10, 11].contains(i))
+            .map(|i| self.buttons_from_bits(i, binary_button_flags))
+            .collect()
+    }
+
+    pub fn buttons_from_bits(&self, i: u8, binary_button_flags: u16) -> TabletButton {
+        let mask = 1 << i;
+        let is_pressed = (binary_button_flags & mask) == 0;
+        let was_pressed = (self.tablet_last_buttons & mask) == 0;
+
+        TabletButton {
+            id: i,
+            state: {
+                match (was_pressed, is_pressed) {
+                    (true, true) => PressState::Hold,
+                    (false, true) => PressState::Pressed,
+                    (false, false) => PressState::None,
+                    (true, false) => PressState::Release,
+                }
+            },
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let device = get_target_device()?;
     let configurations = get_configurations(&device);
-    let interface_descriptors = get_interface_descriptors(&configurations);
+    let interface_descriptors = get_hid_interface_descriptors(&configurations);
 
     let mut device_handler = device.open()?;
     device_handler.set_auto_detach_kernel_driver(true)?;
@@ -39,50 +122,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     signal_flag::register(SIGTERM, Arc::clone(&term))?;
     signal_flag::register(SIGQUIT, Arc::clone(&term))?;
 
+    let mut device_dispatcher = DeviceDispatcher::new();
+
     let mut buf = vec![0u8; 64];
     while !term.load(Ordering::Relaxed) {
         match device_handler.read_interrupt(endpoint_address, &mut buf, Duration::from_secs(3)) {
             Ok(bytes_read) => {
-                debug_buffer(&buf[..bytes_read]);
+                device_dispatcher.dispatch(&buf[..bytes_read]);
             }
             Err(_e) => (),
         }
     }
 
     Ok(())
-}
-
-fn debug_buffer(buffer: &[u8]) -> () {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    println!("");
-    let mut counter = 0;
-    let mut position = 0;
-    for byte in buffer {
-        if [1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14].contains(&position) {
-            print!("--- ");
-        } else {
-            print!("{:03} ", byte);
-        }
-        counter += 1;
-        if counter == 16 {
-            println!("");
-            counter = 0;
-        }
-        position += 1;
-    }
-    println!("---");
-    println!("[01]: {:08} Axis X most", &buffer[01]);
-    println!("[02]: {:08} Axis X last", &buffer[02]);
-    println!("[03]: {:08} Axis Y most", &buffer[03]);
-    println!("[13]: {:08} Axis Y most", &buffer[13]);
-    println!("[04]: {:08} Axis Y last", &buffer[04]);
-    println!("[14]: {:08} Axis Y last", &buffer[14]);
-    println!("[05]: {:08} Pen Pressure most", &buffer[05]);
-    println!("[06]: {:08} Pen Pressure last", &buffer[06]);
-    println!("[09]: {:08b} Pen Btns", &buffer[09]);
-    println!("[10]: {:08b} Pen/Table Proximity", &buffer[10]);
-    println!("[11]: {:08b} Btns", &buffer[11]);
-    println!("[12]: {:08b} Btns", &buffer[12]);
 }
 
 fn set_report(device_handler: &mut DeviceHandle<GlobalContext>) -> Result<(), RusbError> {
@@ -112,7 +164,9 @@ fn get_target_device() -> Result<Device<GlobalContext>, RusbError> {
     }
 }
 
-fn get_interface_descriptors(config_descriptors: &[ConfigDescriptor]) -> Vec<InterfaceDescriptor> {
+fn get_hid_interface_descriptors(
+    config_descriptors: &[ConfigDescriptor],
+) -> Vec<InterfaceDescriptor> {
     config_descriptors
         .iter()
         .flat_map(|config_descriptor| config_descriptor.interfaces())
