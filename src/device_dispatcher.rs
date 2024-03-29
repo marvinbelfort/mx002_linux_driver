a2f55444c8f628a3350a39e2097a353088ff0143
Marcus Vinícius Belfort <marvinbelfort@gmail.com>
2024-03-29 19:44:24 -0300

    Basic Physical vs Emited keys defined

--- src/device_dispatcher.rs
+++ src/device_dispatcher.rs
@@ -1,14 +1,13 @@
-
 use evdev::{uinput::VirtualDeviceBuilder, Key};
+use std::collections::HashMap;
 
 enum PressState {
     Pressed = 0,
     Release = 1,
     Hold = 2,
-    None = 3,
 }
 
-pub struct TabletButton {
+pub struct PhysicalButton {
     id: u8,
     state: PressState,
 }
@@ -16,6 +15,8 @@ pub struct TabletButton {
 pub struct DeviceDispatcher {
     tablet_last_buttons: u16,
     pen_last_buttons: u8,
+    tablet_id_to_key_map: HashMap<u8, Key>,
+    pen_id_to_key_map: HashMap<u8, Key>,
 }
 
 impl DeviceDispatcher {
@@ -31,10 +32,36 @@ impl DeviceDispatcher {
 
     pub fn new() -> Self {
         let tablet_buttons_ids: Vec<u8> = (0..14).filter(|i| ![10, 11].contains(i)).collect();
-        let default_tablet_keys: Vec<Key> = vec![Key::KEY_TAB, Key::KEY_SPACE, Key::KEY_LEFTALT, Key::KEY_LEFTCTRL,];
+        let default_tablet_keys: Vec<Key> = vec![
+            Key::KEY_TAB,
+            Key::KEY_SPACE,
+            Key::KEY_LEFTALT,
+            Key::KEY_LEFTCTRL,
+            Key::KEY_SCROLLDOWN,
+            Key::KEY_SCROLLUP,
+            Key::KEY_LEFTBRACE,
+            Key::KEY_KPMINUS,
+            Key::KEY_KPPLUS,
+            Key::KEY_E,
+            Key::KEY_B,
+            Key::KEY_RIGHTBRACE,
+        ];
+
+        let default_pen_keys: Vec<Key> = vec![
+            Key::BTN_STYLUS,
+            Key::BTN_STYLUS2,
+        ];
+
+        let pen_buttons_ids: Vec<u8> = vec![4, 6];
+
         DeviceDispatcher {
             tablet_last_buttons: 0,
             pen_last_buttons: 0,
+            tablet_id_to_key_map: tablet_buttons_ids
+                .into_iter()
+                .zip(default_tablet_keys.into_iter())
+                .collect(),
+            pen_id_to_key_map: pen_buttons_ids.into_iter().zip(default_pen_keys.into_iter()).collect(),
         }
     }
 
@@ -44,8 +71,12 @@ impl DeviceDispatcher {
             buffer[Self::TABLET_BUTTONS_LOW],
         );
 
-        let buttons = self.binary_to_buttons(binary_button_flags);
+        let tablet_buttons_with_events = self.binary_flags_to_tablet_buttons(binary_button_flags);
         self.tablet_last_buttons = binary_button_flags;
+
+        let pen_button_data = buffer[Self::PEN_BUTTONS];
+        let pen_buttons_with_events = self.pen_button_data_to_pen_buttons(pen_button_data);
+        self.pen_last_buttons = pen_button_data;
 
         // Mapeia botões pressionados para KEY_Events
         // Calcula boundaries e Emite X, Y
@@ -56,28 +87,41 @@ impl DeviceDispatcher {
         ((high | 0xcc) as u16) << 8 | low as u16
     }
 
-    fn binary_to_buttons(&self, binary_button_flags: u16) -> Vec<TabletButton> {
+    fn binary_flags_to_tablet_buttons(&self, binary_button_flags: u16) -> Vec<PhysicalButton> {
         (0..14)
             .filter(|i| ![10, 11].contains(i))
-            .map(|i| self.buttons_from_bits(i, binary_button_flags))
+            .filter_map(|i| self.tablet_button_from_bits(i, binary_button_flags))
             .collect()
     }
 
-    pub fn buttons_from_bits(&self, i: u8, binary_button_flags: u16) -> TabletButton {
+    pub fn tablet_button_from_bits(
+        &self,
+        i: u8,
+        binary_button_flags: u16,
+    ) -> Option<PhysicalButton> {
         let mask = 1 << i;
         let is_pressed = (binary_button_flags & mask) == 0;
         let was_pressed = (self.tablet_last_buttons & mask) == 0;
 
-        TabletButton {
-            id: i,
-            state: {
-                match (was_pressed, is_pressed) {
-                    (true, true) => PressState::Hold,
-                    (false, true) => PressState::Pressed,
-                    (false, false) => PressState::None,
-                    (true, false) => PressState::Release,
-                }
-            },
+        match (was_pressed, is_pressed) {
+            (true, true) => Some(PressState::Hold),
+            (false, true) => Some(PressState::Pressed),
+            (true, false) => Some(PressState::Release),
+            (false, false) => None,
         }
+        .map(|state| PhysicalButton { id: i, state })
+    }
+
+    fn pen_button_data_to_pen_buttons(&self, pen_buttons: u8) -> Option<PhysicalButton> {
+        match (self.pen_last_buttons, pen_buttons) {
+            (2, x) if x == 6 || x == 4 => Some(PressState::Pressed),
+            (x, 2) if x == 6 || x == 4 => Some(PressState::Release),
+            (x, y) if x == y => Some(PressState::Hold),
+            _ => None,
+        }
+        .map(|state| PhysicalButton {
+            id: pen_buttons,
+            state,
+        })
     }
 }
